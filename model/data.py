7da18a3a81ac9bbcc7726f5596765ecc90f3ac86
Dave Brooks <dave@bcs.co.nz>
2012-02-02 15:34:52 +1300

    Rework classes to hold data -- now have Clock, UniformClock, TimeSeries and UniformTimeSeries.

--- model/data.py
+++ model/data.py
@@ -19,68 +19,93 @@ class DataError(Exception):
   pass
 
 
+class Clock(object):
+#===================
+  """
+  The sample times of a :class:`TimeSeries`.
+
+  :param np.array times: :class:`np.array` of sample times, in seconds.
+  """
+  def __init__(self, times):
+  #-------------------------
+    self._times = times
+
+  def __getitem__(self, key):
+  #--------------------------
+    return self._times[key]
+
+  def __len__(self):
+  #-----------------
+    return len(self.__times)
+
+  """
+  Add times to a clock.
+
+  :param np.array times: :class:`np.array` of sample times, in seconds.
+  """
+  def extend(self, times):
+  #-----------------------
+    if self._times[-1] >= times[0]:
+      raise DataError('Times must be increasing')
+    self._times = np.append(self._times, times)
+
+
+class UniformClock(Clock):
+#=========================
+  """
+  The sample times of a :class:`UniformTimeSeries`.
+
+  :param float rate: The sample rate, in Hertz.
+  """
+  def __init__(self, rate):
+  #------------------------
+    self._rate = float(rate)
+
+  def __getitem__(self, key):
+  #--------------------------
+    print key, self._rate
+    if isinstance(key, slice):
+      if key.stop is None: raise TypeError
+      return np.arange(key.start if key.start is not None else 0,
+                       key.stop, key.step)/self._rate
+    elif isinstance(key, int):
+      return key/self._rate
+    else:
+      raise TypeError
+
+  """
+  A UniformClock runs forever -- give a length of 0.
+  """
+  def __len__(self):
+  #-----------------
+    return 0
+
+  """
+  We can not extend a UniformClock.
+  """
+  def extend(self, times):
+  #-----------------------
+    raise DataError('Can not extend a UniformClock')
+
+
 class TimeSeries(object):
 #========================
   """
-  :param np.ndarray data:  the actual data points.
-  :param float starttime: the data block's start time [seconds].
-  :param float startpos: the data block's starting index position.
-  :param float rate:  the data rate of the signal or 0 if sample times are given.
-  :param clock: the sample times for an irregularly sampled signal's data points
-                or a TimeSeries object that has a clock with this signal's sample
-                times.
-  :type  clock: np.ndarray or :class:`TimeSeries` or None
+  A series of data values with times.
 
-  .. note:: We haven't specified how time is represented, apart from assuming that
-            time values are in seconds.
-  """
-  """
-  data    np.array
-  start   float
-  rate    float
-  clock   np.array
-
-  __getitem__(self, i)
-  time(self, i)
+  :param np.array times: :class:`np.array` of sample times, in seconds or a :class:`Clock` with times.
+  :param np.array data: :class:`np.array` of data values.
 
   append(self, data, clock=None)
   extend(self, timeseries)
   """
-
-  ### What are 'starttime' and 'startpos'? Positions on what timeline??
-  ### And their relationship to 'rate'? to 'clock' ??
-
-
-  def __init__(self, data, starttime=None, startpos=None, rate=0, clock=None):
-  #---------------------------------------------------------------------------
-    if   rate < 0 or rate == 0 and clock is None:
-      raise DataError('Time Series timing must be specified')
-    elif rate > 0 and clock is not None:
-      raise DataError('Time Series cannot have both a rate and an explicit clock')
-    elif starttime is None and startpos is None:
-      raise DataError('Either start time or position must be given')
-    elif rate == 0 and (starttime is None or startpos is None):
-      raise DataError('Start time and position must both be given if clock is explicit')
-    elif rate > 0 and starttime is not None and startpos is not None and rate*starttime != startpos:
-      raise DataError("Start time doesn't match position with given rate")
-
-    if   starttime is None: starttime = startpos/rate   ## What if rate is None
-    elif startpos is None: startpos = int(starttime*rate + 0.5) ##  - ditto -
-    self.starttime = starttime
-    self.startpos = startpos
-    self.rate = rate
-    self.period = 1.0/self.rate if self.rate else None
-    self.clock = clock
+  def __init__(self, times, data):
+  #-------------------------------
+    if len(times) != len(data):
+      raise DataError('Number of sample times and data points are different')
     self.data = data
-    if rate == 0 and len(data) != self._timesize():
-      raise DataError("Length of clock doesn't match data length")
-    if len(data):
-      ## Check is no longer meaningful with way time() is calculated...
-      ## if starttime != self.time(0): raise DataError('Time Series start time mismatch')
-      self.endtime = self.time(len(data) - 1)
-    else:
-      self.endtime = self.starttime
-    self.nextstart = self.period*(self.startpos + len(data)) if self.rate else None
+    if isinstance(times, Clock): self.time = times
+    else:                        self.time = Clock(times)
 
   def __len__(self):
   #-----------------
@@ -88,53 +113,138 @@ class TimeSeries(object):
      
   def __str__(self):
   #-----------------
-    return ('<Time Series: rate=%s (p=%s), [start, end]=[%s, %s]>'
-     % (self.rate, self.period, self.starttime, self.endtime))
+    return '<Time Series:\n times=%s\n data=%s>' % (self.times, self.data)
 
-  def __getitem__(self, n):
-  #------------------------
-    return self.data[n]
+  def __getitem__(self, key):
+  #--------------------------
+    if isinstance(key, slice): return np.column_stack((self.time[key], self.data[key]))
+    elif isinstance(key, int): return (self.time[key], self.data[k])
+    else:                      raise TypeError
 
-  def _timesize(self):
-  #-------------------
-    if   self.rate:                          return len(self)
-    elif isinstance(self.clock, TimeSeries): return self.clock._timesize()
-    else:                                    return len(self.clock)
+  @property
+  def times(self):
+  #---------------
+    return self.time[:len(self)]
 
-  def time(self, n):
+  @property
+  def points(self):
+  #----------------
+    return np.column_stack((self.times, self.data))
+
+  """
+  Extend a time series.
+
+  :param np.array data: :class:`np.array` of data values.
+  :param np.array times: :class:`np.array` of sample times, in seconds
+    or a :class:`Clock` with times for the entire, extended, time series.
+  """
+  def extend(self, times, data):
+  #-----------------------------
+    if self.time[-1] >= times[0]:
+      raise DataError('Times must be increasing')
+    if isinstance(times, Clock):
+      if len(data) != (len(times) - len(self.time)):
+        raise DataError('Number of sample times and data points are different')
+      if np.sum(self.time, times[len(self.time):]) != 0.0:
+        raise DataError('Sample times have changed')
+      self.time = times
+    else:
+      if len(times) != len(data):
+        raise DataError('Number of sample times and data points are different')
+      self.time.extend(times)
+    self.data = np.append(self.data, data)
+
+  """
+  Join two time series together.
+
+  :param TimeSeries series: The time series to add.
+  :return: The new :class:`TimeSeries` formed by concatenation.
+  """
+  def __add__(self, series):
+  #-------------------------
+    #if isinstance(series, UniformTimeSeries):
+    #  return TimeSeries(np.concatenate((self.times, series.times + self.time[-1] + 1.0/series.rate)),
+    #                    np.concatenate((self.data, series.data)))
+    if self.time[-1] >= series.time[0]:
+      raise DataError('Times must be increasing')
+      return TimeSeries(np.concatenate((self.times, series.times)),
+                        np.concatenate((self.data, series.data)))
+
+
+class UniformTimeSeries(TimeSeries):
+#===================================
+  """
+  A series of uniformly spaced data values.
+
+  :param np.array data: :class:`np.array` of data values.
+  :param float rate: The sample rate, in Hertz.
+  :param float period: The sampling period, in seconds, can be specified instead of `rate`,
+  """
+  def __init__(self, data, rate=None, period=None):
+  #------------------------------------------------
+    if rate is None and period is None:
+      raise DataError('No sampling rate nor period specified')
+    elif rate is not None and period is not None:
+      raise DataError('Only one of rate or period can be specified')
+    self.data = data
+    self.rate = rate if rate else 1.0/period
+    self.time = UniformClock(self.rate)
+
+  def __str__(self):
   #-----------------
-    """
-    Get the time of a data point.
+    return '<Time Series, rate=%s: %s>' % (self.rate, self.data)
 
-    :param int n: the index of the data point (0-based)
-    :rtype: float
-    """
-    if n < 0 or n >= len(self): raise IndexError
-    if self.period:             return self.period*(self.startpos + n)
-    elif isinstance(self.clock, TimeSeries): return self.clock.time(n)
-    else:                                    return self.clock[n]
+  """
+  Extend a uniform time series.
 
-  def extend(self, next):
+  :param np.array data: :class:`np.array` of data values.
+  """
+  def extend(self, data):
   #----------------------
-    """
-    Extend a TimeSeries with another.
+    self.data = np.append(self.data, data)
 
-    :param next: the next data block.
-    :type  next: :class: TimeSeries
-    """
-    if   next.starttime <= self.endtime:
-      raise DataError('Joined time series must start after current end')
-    elif next.rate != self.rate:
-      raise DataError('Cannot join time series with a different rates or timing')
-    elif next.rate == 0:
-      if (isinstance(next.clock, TimeSeries) and not isinstance(self.clock, TimeSeries)
-       or not isinstance(next.clock, TimeSeries) and isinstance(self.clock, TimeSeries)):
-        raise DataError("Joined time series has different timing")
-    elif self.nextstart and next.starttime != self.nextstart:
-      raise DataError("Joined time series doesn't follow on")
+  """
+  Join two uniform time series together.
 
-    self.data = np.concatenate((self.data, next.data))
-    if next.rate == 0 and not isinstance(next.clock, TimeSeries):
-      self.clock = np.concatenate((self.clock, next.clock))
-    self.endtime = next.endtime
-    if self.rate: self.nextstart = self.period*(self.startpos + len(data))
+  :param TimeSeries series: The uniform time series to add.
+  :return: The new :class:`UniformTimeSeries` formed by concatenation.
+  """
+  def __add__(self, series):
+  #-------------------------
+    if not isinstance(series, UniformTimeSeries):
+      raise TypeError('Can only concatenate with another UniformTimeSeries')
+    if self.rate != series.rate:
+      raise TypeError('Sample rates are different')
+    return UniformTimeSeries(np.concatenate((self.data, series.data)), self.rate)
+
+
+if __name__ == '__main__':
+#-------------------------
+
+  import math
+
+  t = np.linspace(0.0, 2.0*math.pi, 101)
+
+  sw = TimeSeries(t, np.sin(t))
+
+  uts = UniformTimeSeries(np.cos(t), 1000)
+
+  np.set_printoptions(threshold=50, edgeitems=5, suppress=True)
+
+  print sw
+  print uts
+  print uts.points
+
+  sw.extend(uts.times + sw.time[-1] + 1.0/uts.rate, uts.data)
+
+  j = sw
+
+  print j
+  print j.points
+
+  print j[95:106]
+
+
+#  import pylab
+#  pylab.plot(j.times, j.data)
+#  pylab.show()
