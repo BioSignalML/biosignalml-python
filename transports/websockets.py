7bd0ef45aacba80467f538f594eca355472f4c51
Dave Brooks <dave@bcs.co.nz>
2012-02-27 15:10:22 +1300

    Tweak documentation and use renamed stream classes.

--- transports/websockets.py
+++ transports/websockets.py
@@ -26,17 +26,17 @@ class WebSocketReadStream(ws4py.client.threadedclient.WebSocketClient):
   A web socket connection.
   
   We send a data request when the connection is established and then then parse received data.
-  Stream blocks found are put on to `receiveQ`.
+  Received Stream Blocks found are put to `receiveQ`.
 
   :param endpoint: The URL of the data stream server's endpoint.
   :type endpoint: str
-  :param request: Text to end as a data request.
-  :type request: str
+  :param request: Send to request a response.
+  :type request:  :class:`~biosignalml.transports.stream.StreamBlock`
   :param receiveQ: A queue on which to put complete `StreamBlock`'s. `None` is
    sent when the stream has finished.
   :type receiveQ: :class:`Queue.Queue`
-  :param check: How any checksun is treated. Default `Checksum.CHECK`
-  :type check: :class:`Checksum`
+  :param check: How any checksum is treated. Default `Checksum.CHECK`
+  :type check: :class:`~biosignalml.transports.stream.Checksum`
   """
   def __init__(self, endpoint, request, receiveQ, check=stream.Checksum.CHECK, **kwds):
   #------------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ class WebSocketReadStream(ws4py.client.threadedclient.WebSocketClient):
 
   def opened(self):
   #----------------
-    self.send(self._request)
+    self.send(self._request.bytes(), True)
 
   def closed(self, code, reason=None):
   #-----------------------------------
@@ -59,24 +59,30 @@ class WebSocketReadStream(ws4py.client.threadedclient.WebSocketClient):
 
 
 
-class WebStreamReader(stream.BlockStreamReader):
-#===============================================
+class WebStreamReader(stream.SignalDataStream):
+#==============================================
   """
-  An `iterator` yielding :class:`StreamData` objects from a data stream server via Web Sockets.
+  An `iterator` yielding :class:`~biosignalml.transports.stream.SignalData`
+  objects from a data stream server via Web Sockets.
 
   :param endpoint: The URL of the data stream server's endpoint.
   :type endpoint: str
-  :param uri: The URI of a :class:`biosignalml.model.Recording` or :class:`biosignalml.model.Signal`
-    from which to get :class:`biosignalml.model.data.TimeSeries` data,
-  :type uri: str
+  :param uri: The URI of a :class:`~biosignalml.model.Recording` or of one or more
+    :class:`~biosignalml.model.Signal`\s from which to get :class:`~biosignalml.model.data.TimeSeries`
+    data,
+  :type uri: str or list[str]
   :param start: The time, in seconds, that the first data point will be at or immediately after,
-  :type start: float
-  :param duration: The maximum duration, in seconds, of the :class:`TimeSeries` returned.
+  :type start: float or None
+  :param offset: The index of the first data point. An `offset` can only be given when data from a
+    single signal is requested, and cannot be specified along with `start`.
+  :type offset: integer or None
+  :param duration: The duration, in seconds, of the :class:`~biosignalml.transports.stream.TimeSeries`
+    returned. A value of -1 means to return the complete time series of the signal(s).
   :type duration: float
   """
-  def __init__(self, endpoint, uri, start, duration):
-  #--------------------------------------------------
-    stream.BlockStreamReader.__init__(self, endpoint, uri, start, duration)
+  def __init__(self, endpoint, uri, start=None, offset=None, duration=-1):
+  #-----------------------------------------------------------------------
+    stream.SignalDataStream.__init__(self, endpoint, uri, start, offset, duration)
     try:
       self._ws = WebSocketReadStream(endpoint, self._request, self._receiveQ, protocols=['biosignalml-ssf'])
       self._ws.connect()
@@ -100,5 +106,6 @@ if __name__ == '__main__':
     print 'Usage: %s endpoint uri start duration' % sys.argv[0]
     sys.exit(1)
 
-  for d in WebStreamReader(sys.argv[1], sys.argv[2], float(sys.argv[3]), float(sys.argv[4]) ):
+  for d in WebStreamReader(sys.argv[1], sys.argv[2], start=float(sys.argv[3]),
+                                                     duration=float(sys.argv[4]) ):
     print d
