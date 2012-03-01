872aeff7bce38adf3ff84d1249582e20677f47d8
Dave Brooks <dave@bcs.co.nz>
2012-03-01 16:09:23 +1300

    Export StreamException from 'streams' sub-package.

--- transports/__init__.py
+++ transports/__init__.py
@@ -1,5 +1,17 @@
+######################################################
+#
+#  BioSignalML Project
+#
+#  Copyright (c) 2010-2012  David Brooks
+#
+#  $ID$
+#
+######################################################
+
+
 '''
 Transports for BioSignalML data.
 '''
 
 from websockets import WebStreamReader
+from stream import StreamException
