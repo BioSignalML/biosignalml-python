5cecfcb21c50af8dd1b966fb023b819889e868dd
Dave Brooks <dave@bcs.co.nz>
2012-03-07 13:26:42 +1300

    Put generic AbstractObject class into 'biosignalml.model.core' module.

---
+++ model/core.py
@@ -0,0 +1,217 @@
+######################################################
+#
+#  BioSignalML Management in Python
+#
+#  Copyright (c) 2010-2011  David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  $ID: bbd3c04 on Wed Jun 8 16:47:09 2011 +1200 by Dave Brooks $
+#
+######################################################
+'''
+Abstract BioSignalML objects.
+'''
+
+import uuid
+import logging
+
+import biosignalml.rdf as rdf
+
+
+class AbstractObject(object):
+#============================
+  '''
+  A general abstract resource with metadata.
+
+  :param uri: URI of the resource,
+  :type uri: str
+  :param metadata: Dictionary containing metadata values for the resource.
+  :type metadata: dict
+
+  Resource properties with names in the :attr:`attributes` list are stored as attributes
+  of the class instance; other elements are stored in the :attr:`metadata` dictionary.
+  '''
+
+  metaclass = None
+  '''Class in BioSignalML Ontology as a :class:`biosignalml.rdf.Resource`'''
+
+  attributes = [ 'uri', 'description' ]
+  '''List of generic attributes all resources have.'''
+
+  def __init__(self, uri, metadata={}):
+  #------------------------------------
+    self.metadata = { }
+    '''Dictionary of property values with names not in :attr:`attributes` list.'''
+    self.set_attributes(metadata)
+    self.uri = rdf.Uri(uri)
+    self.graph = None
+
+  def __str__(self):
+  #-----------------
+    return '%s: %s' % (self.__class__, self.uri)
+
+  def initialise(self, *args):
+  #---------------------------
+    pass
+
+  @classmethod
+  def initialise_class(cls, obj, *args):
+  #-------------------------------------
+    if obj.__class__ not in cls.__mro__:
+      raise TypeError('Object not in superclasses')
+    obj.__class__ = cls
+    cls.initialise(obj, *args)
+    return obj
+
+  def set_attributes(self, values):
+  #--------------------------------
+    '''
+    Set attribute if `key` exists in an :attr:`attributes` list of any class in hierarchy.
+
+    :param meta: Dictionary of `{ key: value }` pairs to set as attributes.
+    :type meta: dict
+    '''
+    assigned = [ ]
+    for cls in self.__class__.__mro__:
+      if 'attributes' in cls.__dict__:
+        for attr in cls.__dict__['attributes']:
+          value = values.get(attr)
+          if value is not None:
+            setattr(self, attr, value)
+            assigned.append(attr)
+          elif attr not in self.__dict__:
+            setattr(self, attr, None)    # So it's able to be _assign()ed to
+    for attr, value in values.iteritems():
+      if attr not in assigned: self.metadata[attr] = value
+
+  def get_attributes(self):
+  #------------------------
+    '''
+    Get values of attributes if defined in :attr:`attributes` list of class hierarchy.
+
+    :return: Dictionary of { key: value } pairs of attribute values.
+    :rtype: dict
+    '''
+    metadata = { }
+    for cls in self.__class__.__mro__:
+      if 'attributes' in cls.__dict__:
+        for attr in cls.__dict__['attributes']:
+          value = getattr(self, attr, None)
+          if value is not None: metadata[attr] = value
+    return metadata
+
+  def make_uri(self, sibling=False):
+  #---------------------------------
+    """
+    Generate a unique URI that starts with the resource's URI.
+
+    :param sibling: When set, replace the last component of our URI with unique text.
+      The default is to append unique text to our URI.
+    :type: bool
+    :return: A unique URI.
+    :rtype: str
+    """
+    u = str(self.uri)
+    if   u.endswith(('/', '#')): return '%s%s'  % (u, uuid.uuid1())
+    elif sibling:
+      slash = u.rfind('/')
+      hash  = u.rfind('#')
+      if hash > slash:           return '%s#%s' % (u.rsplit('#', 1)[0], uuid.uuid1())
+      else:                      return '%s/%s' % (u.rsplit('/', 1)[0], uuid.uuid1())
+    else:                        return '%s/%s' % (u, uuid.uuid1())
+
+  def save_to_graph(self, graph, rdfmap=None):
+  #-------------------------------------------
+    '''
+    Add RDF statements about ourselves to a graph.
+
+    '''
+    import mapping
+    if rdfmap is None: rdfmap = mapping.bsml_mapping()
+    if (self.metaclass):
+      graph.append(rdf.Statement(self.uri, rdf.RDF.type, self.metaclass))
+      graph.add_statements(rdfmap.statement_stream(self))
+
+  def metadata_as_graph(self, rdfmap=None):
+  #----------------------------------------
+    """
+    Return metadata in a RDF graph.
+    """
+    graph = rdf.Graph(self.uri)
+    self.save_to_graph(graph, rdfmap)
+    return graph
+
+  def metadata_as_string(self, format=rdf.Format.TURTLE, prefixes={ }, rdfmap=None):
+  #---------------------------------------------------------------------------------
+    """
+    Return metadata as a serialised RDF string.
+    """
+    namespaces = { 'bsml': BSML.uri }
+    namespaces.update(rdf.NAMESPACES)
+    namespaces.update(prefixes)
+    return self.metadata_as_graph(rdfmap=rdfmap).serialise(base=str(self.uri) + '/',
+                                  format=format, prefixes=namespaces)
+
+  def _assign(self, attr, value):
+  #------------------------------
+    if attr in self.__dict__: setattr(self, attr, value)
+    else:                     self.metadata[attr] = value
+
+  def load_from_graph(self, graph, rdfmap=None):
+  #---------------------------------------------
+    """
+    Set attributes from RDF triples in a graph.
+
+    :param graph: A graph of RDF statements.
+    :type graph: :class:`biosignalml.rdf.Graph`
+    :param rdfmap: How to map properties to attributes.
+    :type rdfmap: :class:`~biosignalml.model.mapping.Mapping`
+    """
+    import mapping
+    if rdfmap is None: rdfmap = mapping.bsml_mapping()
+    if graph.contains(rdf.Statement(self.uri, rdf.RDF.type, self.metaclass)):
+      for stmt in graph.get_statements(rdf.Statement(self.uri, None, None)):
+        s, attr, v = rdfmap.metadata(stmt, self.metaclass) # Need to go up __mro__
+        ##logging.debug("%s: %s='%s'", self.uri, attr, v)  ###
+        self._assign(attr, v)
+
+  @classmethod
+  def create_from_graph(cls, uri, graph, rdfmap=None, **kwds):
+  #-----------------------------------------------------------
+    '''
+    Create a new instance of a resource, setting attributes from RDF triples in a graph.
+
+    :param uri: The URI for the resource.
+    :param graph: A RDF graph.
+    :type graph: :class:`~biosignalml.rdf.Graph`
+    :param rdfmap: How to map properties to attributes.
+    :type rdfmap: :class:`~biosignalml.model.mapping.Mapping`
+    :rtype: :class:`AbstractObject`
+    '''
+    self = cls(uri, **kwds)
+    self.load_from_graph(graph, rdfmap)
+    self.graph = graph
+    return self
+
+  def set_from_graph(self, attr, graph, rdfmap=None):
+  #--------------------------------------------------
+    '''
+    Set an attribute from RDF statement in the form `(uri, attr, value)`.
+    '''
+    import mapping
+    if rdfmap is None: rdfmap = mapping.bsml_mapping()
+    v = rdfmap.get_value_from_graph(self.uri, attr, graph)
+    if v: self._assign(attr, v)
+
+
