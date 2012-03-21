5bfae90aefe020441de65c20ef0cafd44a01cc95
Dave Brooks <dave@bcs.co.nz>
2012-03-21 17:12:40 +1300

    Generate resources for UNITS from Units of Measurement List.

---
+++ units/__init__.py
@@ -0,0 +1,690 @@
+"""
+Provide access to the OUME List ontology.
+
+Generated from file:///Users/dave/biosignalml/ontologies/uome/list.ttl at Wed Mar 21 14:39:12 2012
+"""
+
+from biosignalml.rdf import Uri, Resource, NS as Namespace
+
+class UNIT(object):
+  uri = Uri("http://www.sbpax.org/uome/list.owl#")
+  NS = Namespace(str(uri))
+
+  Absorbance     = Resource(NS.Absorbance)
+  '''absorbance (A)'''
+
+  Ampere         = Resource(NS.Ampere)
+  '''ampere (A)'''
+
+  Angstrom       = Resource(NS.Angstrom)
+  '''angstrom (Å)'''
+
+  Atmosphere     = Resource(NS.Atmosphere)
+  '''atmosphere (atm)'''
+
+  Attomole       = Resource(NS.Attomole)
+  '''attomole (amol)'''
+
+  BasePairs      = Resource(NS.BasePairs)
+  '''base pairs (base pairs)'''
+
+  Becquerel      = Resource(NS.Becquerel)
+  '''becquerel (Bq)'''
+
+  Bel            = Resource(NS.Bel)
+  '''bel (B)'''
+
+  Bit            = Resource(NS.Bit)
+  '''bit (bit)'''
+
+  Byte           = Resource(NS.Byte)
+  '''byte (B)'''
+
+  Candela        = Resource(NS.Candela)
+  '''candela (cd)'''
+
+  CandelaPerSquareMetre = Resource(NS.CandelaPerSquareMetre)
+  '''candela per square metre (C/m^2)'''
+
+  Centimetre     = Resource(NS.Centimetre)
+  '''centimetre (cm)'''
+
+  Century        = Resource(NS.Century)
+  '''century (century)'''
+
+  Coulomb        = Resource(NS.Coulomb)
+  '''coulomb (C)'''
+
+  CoulombPerKilogram = Resource(NS.CoulombPerKilogram)
+  '''coulomb per kilogram (C/kg)'''
+
+  Count          = Resource(NS.Count)
+  '''count ()'''
+
+  CountPerMillilitre = Resource(NS.CountPerMillilitre)
+  '''count per millilitre (1/ml)'''
+
+  CountPerMolarSecond = Resource(NS.CountPerMolarSecond)
+  '''count per molar second (1/(M*s))'''
+
+  CountPerNanomolarSecond = Resource(NS.CountPerNanomolarSecond)
+  '''count per nanomolar second (1/(nM*s))'''
+
+  CountsPerMinute = Resource(NS.CountsPerMinute)
+  '''counts per minute (cpm)'''
+
+  CubicCentimetre = Resource(NS.CubicCentimetre)
+  '''cubic centimetre (cm^3)'''
+
+  CubicCentimetrePerMole = Resource(NS.CubicCentimetrePerMole)
+  '''cubic centimetre per mole (cm^3/mol)'''
+
+  CubicDecimetre = Resource(NS.CubicDecimetre)
+  '''cubic decimetre (dm^3)'''
+
+  CubicMetre     = Resource(NS.CubicMetre)
+  '''cubic metre (m^3)'''
+
+  CubicMetrePerKilogram = Resource(NS.CubicMetrePerKilogram)
+  '''cubic metre per kilogram (m^3/kg)'''
+
+  CubicMetrePerMole = Resource(NS.CubicMetrePerMole)
+  '''cubic metre per mole (m^3/mol)'''
+
+  Curie          = Resource(NS.Curie)
+  '''curie (Ci)'''
+
+  Dalton         = Resource(NS.Dalton)
+  '''dalton (Da)'''
+
+  Day            = Resource(NS.Day)
+  '''day (d)'''
+
+  Decibel        = Resource(NS.Decibel)
+  '''decibel (dB)'''
+
+  Decilitre      = Resource(NS.Decilitre)
+  '''decilitre (dl)'''
+
+  Decimetre      = Resource(NS.Decimetre)
+  '''decimetre (dm)'''
+
+  DegreeCelsius  = Resource(NS.DegreeCelsius)
+  '''degree celsius (°C)'''
+
+  DegreeCelsiusScaledToFahrenheit = Resource(NS.DegreeCelsiusScaledToFahrenheit)
+  '''degree celsius scaled to fahrenheit (1.8 °C)'''
+
+  DegreeFahrenheit = Resource(NS.DegreeFahrenheit)
+  '''degree fahrenheit (°F)'''
+
+  DegreeOfArc    = Resource(NS.DegreeOfArc)
+  '''degree of arc (°)'''
+
+  Dimensionless  = Resource(NS.Dimensionless)
+  '''Dimensionless ()'''
+
+  DisintegrationsPerMinute = Resource(NS.DisintegrationsPerMinute)
+  '''disintegrations per minute (dpm)'''
+
+  DisintegrationsPerSecond = Resource(NS.DisintegrationsPerSecond)
+  '''disintegrations per second (disintegrations per second)'''
+
+  DotsPerInch    = Resource(NS.DotsPerInch)
+  '''dots per inch (DPI)'''
+
+  Dyne           = Resource(NS.Dyne)
+  '''dyne (dyn)'''
+
+  DynePerCentimetre = Resource(NS.DynePerCentimetre)
+  '''dyne per centimetre (dyn/cm)'''
+
+  Einstein       = Resource(NS.Einstein)
+  '''einstein (einstein)'''
+
+  EinsteinPerSquareMetrePerSecond = Resource(NS.EinsteinPerSquareMetrePerSecond)
+  '''einstein per square metre per second (einstein/(m^2s))'''
+
+  Electronvolt   = Resource(NS.Electronvolt)
+  '''electronvolt (eV)'''
+
+  ElementaryCharge = Resource(NS.ElementaryCharge)
+  '''elementary charge (e)'''
+
+  EnzymeUnit     = Resource(NS.EnzymeUnit)
+  '''enzyme unit (U)'''
+
+  Farad          = Resource(NS.Farad)
+  '''farad (F)'''
+
+  Femtogram      = Resource(NS.Femtogram)
+  '''femtogram (fg)'''
+
+  Femtolitre     = Resource(NS.Femtolitre)
+  '''femtolitre (fl)'''
+
+  Femtomolar     = Resource(NS.Femtomolar)
+  '''femtomolar (fM)'''
+
+  Femtomole      = Resource(NS.Femtomole)
+  '''femtomole (pmol)'''
+
+  Foot           = Resource(NS.Foot)
+  '''foot (ft)'''
+
+  FootCandle     = Resource(NS.FootCandle)
+  '''foot-candle (fc)'''
+
+  Fraction       = Resource(NS.Fraction)
+  '''fraction ()'''
+
+  Gram           = Resource(NS.Gram)
+  '''gram (g)'''
+
+  GramPerCubicCentimetre = Resource(NS.GramPerCubicCentimetre)
+  '''gram per cubic centimetre (g/cm^3)'''
+
+  GramPerDecilitre = Resource(NS.GramPerDecilitre)
+  '''gram per decilitre (g/dl)'''
+
+  GramPerLitre   = Resource(NS.GramPerLitre)
+  '''gram per litre (g/l)'''
+
+  GramPerMillilitre = Resource(NS.GramPerMillilitre)
+  '''gram per millilitre (g/ml)'''
+
+  GramPerMole    = Resource(NS.GramPerMole)
+  '''gram per mole (g/mol)'''
+
+  Gray           = Resource(NS.Gray)
+  '''gray (Gy)'''
+
+  Hectare        = Resource(NS.Hectare)
+  '''hectare (ha)'''
+
+  Henry          = Resource(NS.Henry)
+  '''henry (H)'''
+
+  Hertz          = Resource(NS.Hertz)
+  '''hertz (Hz)'''
+
+  Hour           = Resource(NS.Hour)
+  '''hour (h)'''
+
+  Inch           = Resource(NS.Inch)
+  '''inch (in)'''
+
+  Joule          = Resource(NS.Joule)
+  '''joule (J)'''
+
+  Katal          = Resource(NS.Katal)
+  '''katal (kat)'''
+
+  KatalPerCubicMetre = Resource(NS.KatalPerCubicMetre)
+  '''katal per cubic metre (kat/m^3)'''
+
+  KatalPerLitre  = Resource(NS.KatalPerLitre)
+  '''katal per litre (kat/l)'''
+
+  Kelvin         = Resource(NS.Kelvin)
+  '''kelvin (K)'''
+
+  Kibibyte       = Resource(NS.Kibibyte)
+  '''kibibyte (KiB)'''
+
+  Kilobyte       = Resource(NS.Kilobyte)
+  '''kilobyte (kB)'''
+
+  Kilodalton     = Resource(NS.Kilodalton)
+  '''kilodalton (kDa)'''
+
+  Kilogram       = Resource(NS.Kilogram)
+  '''kilogram (kg)'''
+
+  KilogramMetre  = Resource(NS.KilogramMetre)
+  '''kilogram metre (kg*m)'''
+
+  KilogramMetrePerSecond = Resource(NS.KilogramMetrePerSecond)
+  '''kilogram metre per second (kg*m/s)'''
+
+  KilogramPerCubicMetre = Resource(NS.KilogramPerCubicMetre)
+  '''kilogram per cubic metre (kg/m^3)'''
+
+  KilogramPerLitre = Resource(NS.KilogramPerLitre)
+  '''kilogram per litre (kg/l)'''
+
+  KilogramPerMetre = Resource(NS.KilogramPerMetre)
+  '''kilogram per metre (kg/m)'''
+
+  KilogramPerMole = Resource(NS.KilogramPerMole)
+  '''kilogram per mole (kg/mol)'''
+
+  KilogramPerSquareMetre = Resource(NS.KilogramPerSquareMetre)
+  '''kilogram per square metre (kg/m^2)'''
+
+  KilogramTimesMetre = Resource(NS.KilogramTimesMetre)
+  '''kilogram times metre (kg*m)'''
+
+  Kilovolt       = Resource(NS.Kilovolt)
+  '''kilovolt (kV)'''
+
+  KilovoltHour   = Resource(NS.KilovoltHour)
+  '''kilovolt-hour (kVh)'''
+
+  Kilowatt       = Resource(NS.Kilowatt)
+  '''kilowatt (kW)'''
+
+  KilowattHour   = Resource(NS.KilowattHour)
+  '''kilowatt-hour (kWh)'''
+
+  Litre          = Resource(NS.Litre)
+  '''litre (l)'''
+
+  LitrePerKilogram = Resource(NS.LitrePerKilogram)
+  '''litre per kilogram (l/kg)'''
+
+  Lumen          = Resource(NS.Lumen)
+  '''lumen (lm)'''
+
+  Lux            = Resource(NS.Lux)
+  '''lux (lx)'''
+
+  Mebibyte       = Resource(NS.Mebibyte)
+  '''mebibyte (MiB)'''
+
+  Megabyte       = Resource(NS.Megabyte)
+  '''megabyte (MB)'''
+
+  Megavolt       = Resource(NS.Megavolt)
+  '''megavolt (MV)'''
+
+  Metre          = Resource(NS.Metre)
+  '''metre (m)'''
+
+  MetreKelvin    = Resource(NS.MetreKelvin)
+  '''metre kelvin (m*K)'''
+
+  MetrePerSecond = Resource(NS.MetrePerSecond)
+  '''metre per second (m/s)'''
+
+  MetrePerSecondSquared = Resource(NS.MetrePerSecondSquared)
+  '''metre per second squared (m/s^2)'''
+
+  MicroEinsteinPerSquareMetrePerSecond = Resource(NS.MicroEinsteinPerSquareMetrePerSecond)
+  '''microeinstein per square metre per second (μeinstein/(m^2s))'''
+
+  Microampere    = Resource(NS.Microampere)
+  '''microampere (μA)'''
+
+  Microcurie     = Resource(NS.Microcurie)
+  '''microcurie (μCi)'''
+
+  Microgram      = Resource(NS.Microgram)
+  '''microgram (μg)'''
+
+  MicrogramPerMillilitre = Resource(NS.MicrogramPerMillilitre)
+  '''microgram per millilitre (μg/ml)'''
+
+  Microgray      = Resource(NS.Microgray)
+  '''microgray (μGy)'''
+
+  Microlitre     = Resource(NS.Microlitre)
+  '''microlitre (μl)'''
+
+  MicrolitrePerKilogram = Resource(NS.MicrolitrePerKilogram)
+  '''microlitre per kilogram (μl/kg)'''
+
+  MicrolitrePerMinute = Resource(NS.MicrolitrePerMinute)
+  '''microlitre per minute (μl/min)'''
+
+  Micrometre     = Resource(NS.Micrometre)
+  '''micrometre (μm)'''
+
+  Micromolal     = Resource(NS.Micromolal)
+  '''micromolal (μmol/kg)'''
+
+  Micromolar     = Resource(NS.Micromolar)
+  '''micromolar (μM)'''
+
+  Micromole      = Resource(NS.Micromole)
+  '''micromole (μmol)'''
+
+  MicromolePerMilligramMinute = Resource(NS.MicromolePerMilligramMinute)
+  '''micromole per milligram minute (μmol/(mg*min))'''
+
+  MicronPixel    = Resource(NS.MicronPixel)
+  '''micron pixel (μm/dot)'''
+
+  Microsecond    = Resource(NS.Microsecond)
+  '''microsecond (μs)'''
+
+  Microsievert   = Resource(NS.Microsievert)
+  '''microsievert (μSv)'''
+
+  Microvolt      = Resource(NS.Microvolt)
+  '''microvolt (μV)'''
+  uV             = Microvolt
+
+
+  Milliampere    = Resource(NS.Milliampere)
+  '''milliampere (mA)'''
+
+  Millicurie     = Resource(NS.Millicurie)
+  '''millicurie (mCi)'''
+
+  Milligram      = Resource(NS.Milligram)
+  '''milligram (mg)'''
+
+  MilligramMinute = Resource(NS.MilligramMinute)
+  '''milligram minute (mg*min)'''
+
+  MilligramPerLitre = Resource(NS.MilligramPerLitre)
+  '''milligram per litre (mg/l)'''
+
+  MilligramPerMillilitre = Resource(NS.MilligramPerMillilitre)
+  '''milligram per millilitre (mg/ml)'''
+
+  Milligray      = Resource(NS.Milligray)
+  '''milligray (mGy)'''
+
+  Millilitre     = Resource(NS.Millilitre)
+  '''millilitre (ml)'''
+
+  MillilitrePerCubicMetre = Resource(NS.MillilitrePerCubicMetre)
+  '''millilitre per cubic metre (ml/m^3)'''
+
+  MillilitrePerKilogram = Resource(NS.MillilitrePerKilogram)
+  '''millilitre per kilogram (ml/kg)'''
+
+  MillilitrePerLitre = Resource(NS.MillilitrePerLitre)
+  '''millilitre per litre (ml/l)'''
+
+  Millimetre     = Resource(NS.Millimetre)
+  '''millimetre (mm)'''
+
+  MillimetresOfMercury = Resource(NS.MillimetresOfMercury)
+  '''millimetres of mercury (mmHg)'''
+
+  Millimolal     = Resource(NS.Millimolal)
+  '''millimolal (mmol/kg)'''
+
+  Millimolar     = Resource(NS.Millimolar)
+  '''millimolar (mM)'''
+
+  Millimole      = Resource(NS.Millimole)
+  '''millimole (mmol)'''
+
+  Millisecond    = Resource(NS.Millisecond)
+  '''millisecond (ms)'''
+
+  Millisievert   = Resource(NS.Millisievert)
+  '''millisievert (mSv)'''
+
+  Millivolt      = Resource(NS.Millivolt)
+  '''millivolt (mV)'''
+  mV             = Millivolt
+
+
+  Minute         = Resource(NS.Minute)
+  '''minute (min)'''
+
+  MinuteOfArc    = Resource(NS.MinuteOfArc)
+  '''minute of arc (')'''
+
+  Molal          = Resource(NS.Molal)
+  '''molal (mol/kg)'''
+
+  Molar          = Resource(NS.Molar)
+  '''molar (M)'''
+
+  MolarSecond    = Resource(NS.MolarSecond)
+  '''molar second (M*s)'''
+
+  Mole           = Resource(NS.Mole)
+  '''mole (mol)'''
+
+  MoleFraction   = Resource(NS.MoleFraction)
+  '''mole fraction ()'''
+
+  MolePerMilligram = Resource(NS.MolePerMilligram)
+  '''mole per milligram (mol/mg)'''
+
+  MolePerMilligramMinute = Resource(NS.MolePerMilligramMinute)
+  '''mole per milligram minute (mol/(mg*min))'''
+
+  MoleculeCount  = Resource(NS.MoleculeCount)
+  '''molecule count ()'''
+
+  Month          = Resource(NS.Month)
+  '''month (mon)'''
+
+  Nanogram       = Resource(NS.Nanogram)
+  '''nanogram (ng)'''
+
+  NanogramPerMillilitre = Resource(NS.NanogramPerMillilitre)
+  '''nanogram per millilitre (ng/ml)'''
+
+  Nanogray       = Resource(NS.Nanogray)
+  '''nanogray (nGy)'''
+
+  Nanolitre      = Resource(NS.Nanolitre)
+  '''nanolitre (nl)'''
+
+  Nanometre      = Resource(NS.Nanometre)
+  '''nanometre (nm)'''
+
+  Nanomolal      = Resource(NS.Nanomolal)
+  '''nanomolal (nmol/kg)'''
+
+  Nanomolar      = Resource(NS.Nanomolar)
+  '''nanomolar (nM)'''
+
+  NanomolarSecond = Resource(NS.NanomolarSecond)
+  '''nanomolar second (nM*s)'''
+
+  Nanomole       = Resource(NS.Nanomole)
+  '''nanomole (nmol)'''
+
+  Nanosecond     = Resource(NS.Nanosecond)
+  '''nanosecond (ns)'''
+
+  Nanosievert    = Resource(NS.Nanosievert)
+  '''nanosievert (nSv)'''
+
+  Nanovolt       = Resource(NS.Nanovolt)
+  '''nanovolt (nV)'''
+
+  Newton         = Resource(NS.Newton)
+  '''newton (kg*m/s^2)'''
+
+  NewtonPerMetre = Resource(NS.NewtonPerMetre)
+  '''newton per metre (N/m)'''
+
+  Normal         = Resource(NS.Normal)
+  '''normal (N)'''
+
+  Ohm            = Resource(NS.Ohm)
+  '''ohm (Ω)'''
+
+  PH             = Resource(NS.PH)
+  '''pH ()'''
+
+  PartsPerBillion = Resource(NS.PartsPerBillion)
+  '''parts per billion (ppb)'''
+
+  PartsPerHundred = Resource(NS.PartsPerHundred)
+  '''parts per hundred (%)'''
+
+  PartsPerMillion = Resource(NS.PartsPerMillion)
+  '''parts per million (ppm)'''
+
+  PartsPerQuadrillion = Resource(NS.PartsPerQuadrillion)
+  '''parts per quadrillion (ppt)'''
+
+  PartsPerThousand = Resource(NS.PartsPerThousand)
+  '''parts per thousand (‰)'''
+
+  PartsPerTrillion = Resource(NS.PartsPerTrillion)
+  '''parts per trillion (ppt)'''
+
+  Pascal         = Resource(NS.Pascal)
+  '''pascal (Pa)'''
+
+  PascalSecond   = Resource(NS.PascalSecond)
+  '''pascal second (Pa*s)'''
+
+  PerMinute      = Resource(NS.PerMinute)
+  '''per minute (1/min)'''
+
+  PerSecond      = Resource(NS.PerSecond)
+  '''per second (1/s)'''
+
+  Percent        = Resource(NS.Percent)
+  '''percent (%)'''
+
+  Pi             = Resource(NS.Pi)
+  '''pi (π)'''
+
+  Picogram       = Resource(NS.Picogram)
+  '''picogram (pg)'''
+
+  Picolitre      = Resource(NS.Picolitre)
+  '''picolitre (pl)'''
+
+  Picometre      = Resource(NS.Picometre)
+  '''picometre (pm)'''
+
+  Picomolal      = Resource(NS.Picomolal)
+  '''picomolal (pmol/kg)'''
+
+  Picomolar      = Resource(NS.Picomolar)
+  '''Picomolar (pM)'''
+
+  Picomole       = Resource(NS.Picomole)
+  '''picomole (pmol)'''
+
+  Picosecond     = Resource(NS.Picosecond)
+  '''picosecond (ps)'''
+
+  Picosiemens    = Resource(NS.Picosiemens)
+  '''picosiemens (pS)'''
+
+  Picovolt       = Resource(NS.Picovolt)
+  '''picovolt (pV)'''
+
+  PixelsPerInch  = Resource(NS.PixelsPerInch)
+  '''pixels per inch (dots/in)'''
+
+  PixelsPerMillimetre = Resource(NS.PixelsPerMillimetre)
+  '''pixels per millimetre (dots/mm)'''
+
+  Poise          = Resource(NS.Poise)
+  '''poise (P)'''
+
+  Rad            = Resource(NS.Rad)
+  '''rad (rad)'''
+
+  Radian         = Resource(NS.Radian)
+  '''radian (rad)'''
+
+  RadianPerSecond = Resource(NS.RadianPerSecond)
+  '''radian per second (rad/s)'''
+
+  RadianPerSecondSquared = Resource(NS.RadianPerSecondSquared)
+  '''radian per second squared (rad/s^2)'''
+
+  Ratio          = Resource(NS.Ratio)
+  '''ratio ()'''
+
+  Roentgen       = Resource(NS.Roentgen)
+  '''roentgen (R)'''
+
+  RoentgenEquivalentMan = Resource(NS.RoentgenEquivalentMan)
+  '''roentgen equivalent man (rem)'''
+
+  Second         = Resource(NS.Second)
+  '''second (s)'''
+
+  SecondOfArc    = Resource(NS.SecondOfArc)
+  '''second of arc (")'''
+
+  SecondSquared  = Resource(NS.SecondSquared)
+  '''second squared (s^2)'''
+
+  Siemens        = Resource(NS.Siemens)
+  '''Siemens (S)'''
+
+  Sievert        = Resource(NS.Sievert)
+  '''sievert (Sv)'''
+
+  SquareCentimetre = Resource(NS.SquareCentimetre)
+  '''square centimetre (cm^2)'''
+
+  SquareFoot     = Resource(NS.SquareFoot)
+  '''square foot (ft^2)'''
+
+  SquareMetre    = Resource(NS.SquareMetre)
+  '''square metre (m^2)'''
+
+  SquareMetreSecond = Resource(NS.SquareMetreSecond)
+  '''square metre second (m^2s)'''
+
+  SquareMillimetre = Resource(NS.SquareMillimetre)
+  '''square millimetre (mm^2)'''
+
+  Steradian      = Resource(NS.Steradian)
+  '''steradian (sr)'''
+
+  SteradianSquareMetre = Resource(NS.SteradianSquareMetre)
+  '''steradian square metre (sr*m^2)'''
+
+  Tesla          = Resource(NS.Tesla)
+  '''tesla (T)'''
+
+  Tonne          = Resource(NS.Tonne)
+  '''tonne (t)'''
+
+  Torr           = Resource(NS.Torr)
+  '''torr (Torr)'''
+
+  TurnsPerSecond = Resource(NS.TurnsPerSecond)
+  '''turns per second (1/s)'''
+
+  Volt           = Resource(NS.Volt)
+  '''volt (V)'''
+
+  VoltHour       = Resource(NS.VoltHour)
+  '''volt-hour (Vh)'''
+
+  VoltPerMetre   = Resource(NS.VoltPerMetre)
+  '''volt per metre (V/m)'''
+
+  VoltSecond     = Resource(NS.VoltSecond)
+  '''volt second (V*s)'''
+
+  Watt           = Resource(NS.Watt)
+  '''watt (W)'''
+
+  WattHour       = Resource(NS.WattHour)
+  '''watt-hour (Wh)'''
+
+  WattPerMetreKelvin = Resource(NS.WattPerMetreKelvin)
+  '''watt per metre kelvin (W/(m*K))'''
+
+  WattPerSquareMetre = Resource(NS.WattPerSquareMetre)
+  '''watt per square metre (W/m^2)'''
+
+  WattPerSteradian = Resource(NS.WattPerSteradian)
+  '''watt per steradian (W/sr)'''
+
+  WattPerSteradianPerSquareMetre = Resource(NS.WattPerSteradianPerSquareMetre)
+  '''watt per steradian per square metre (W/(sr*m^2))'''
+
+  Weber          = Resource(NS.Weber)
+  '''weber (Wb)'''
+
+  Week           = Resource(NS.Week)
+  '''week (week)'''
+
+  Year           = Resource(NS.Year)
+  '''year (a)'''
