d6a08b24fc0930988547c36838f8eb599c3fb17e
Dave Brooks <dave@bcs.co.nz>
2013-02-11 22:46:04 +1300

    Add required packages.

--- setup.py
+++ setup.py
@@ -2,18 +2,35 @@
 
 from setuptools import setup, find_packages
 
-version = '0.90'
+version = '0.3.0'
 
 setup(name='BioSignalML',
+
       version=version,
+
       provides=['biosignalml'],
+
       description='Python BioSignal Abstraction Library',
+
       author='David Brooks',
       author_email='d.brooks@auckland.ac.nz',
       url='http://www.biosignalml.org/',
-      packages=find_packages(exclude=['scope',
-                                      'biosignalml.formats.sdf',
+
+#    license = "PSF",
+#    keywords = "hello world example examples",
+
+      zip_safe = True,
+
+      packages=find_packages(exclude=['biosignalml.formats.sdf',
                                       'biosignalml.formats.stream',
                                       'biosignalml.formats.unemap',
                                      ]),
-     )
+      install_requires=['isodate  >= 0.4.7',
+                        'pyparsing == 1.5.7',
+                        'httplib2 >= 0.7.2',
+                        'python-dateutil >= 2.0',
+                        'h5py >= 2.0.1',
+                        'ws4py >= 0.2.4',
+##                        'docutils >= 0.3',  ## For ReST documentation
+                        ],
+      )
