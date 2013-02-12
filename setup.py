d7320d146572aa86722fbb5432f029e587f893d6
Dave Brooks <dave@bcs.co.nz>
2013-02-12 13:12:40 +1300

    Update to install our version of 'pint'; add classifiers, license, etc.

--- setup.py
+++ setup.py
@@ -5,22 +5,30 @@ from setuptools import setup, find_packages
 version = '0.3.0'
 
 setup(name='BioSignalML',
-
       version=version,
-
       provides=['biosignalml'],
 
       description='Python BioSignal Abstraction Library',
-
+#     keywords = "hello world example examples",
       author='David Brooks',
       author_email='d.brooks@auckland.ac.nz',
       url='http://www.biosignalml.org/',
 
-#    license = "PSF",
-#    keywords = "hello world example examples",
+      license = "ASL",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                  ],
 
       zip_safe = True,
-
       packages=find_packages(exclude=['biosignalml.formats.sdf',
                                       'biosignalml.formats.stream',
                                       'biosignalml.formats.unemap',
@@ -31,6 +39,7 @@ setup(name='BioSignalML',
                         'python-dateutil >= 2.0',
                         'h5py >= 2.0.1',
                         'ws4py >= 0.2.4',
-##                        'docutils >= 0.3',  ## For ReST documentation
-                        ],
+                        'pint == 0.1.3-djb',
+                       ],
+      dependency_links=['http://github.com/dbrnz/pint/tarball/master#egg=pint-0.1.3-djb']
       )
