1832172d90d464f7be98cfee2e5fc9dcaba57504
Dave Brooks <dave@bcs.co.nz>
2013-02-15 22:34:18 +1300

    Revert "Define a FINISHED stream block class."
    This reverts commit 8d4adad610b48969eedcd9900ad97e3b991b9f96.

--- biosignalml/transports/stream.py
+++ biosignalml/transports/stream.py
@@ -221,13 +221,6 @@ class BlockType(object):
   :class:`StreamException` to be raised.
   """
 
-  FINISHED = 'F'
-  """
-  The stream is finished with and can be closed.
-
-  **FUTURE** Provide a total block count, reason, etc.
-  """
-
 
 class StreamException(Exception):
 #================================
@@ -357,17 +350,6 @@ class InfoBlock(StreamBlock):
   def __init__(self, **header):
   #----------------------------
     StreamBlock.__init__(self, 0, BlockType.INFO, header, '')
-
-
-class Finished(StreamBlock):
-#===========================
-  """
-  Terminate a block stream.
-
-  """
-  def __init__(self, **header):
-  #----------------------------
-    StreamBlock.__init__(self, 0, BlockType.FINISHED, header, '')
 
 
 class RDFBlock(StreamBlock):
