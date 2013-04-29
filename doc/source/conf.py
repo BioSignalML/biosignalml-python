bd6f05353d66b067aaac19b61bb74b38aa7fdb3e
Dave Brooks <dave@bcs.co.nz>
2013-04-29 18:15:20 +1200

    Update Makefile and configuration settings for changed directory layout.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ extensions = ['sphinx.ext.autodoc',
 todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -75,7 +75,7 @@ release = ''
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -137,7 +137,7 @@ html_style = 'biosignalml.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
