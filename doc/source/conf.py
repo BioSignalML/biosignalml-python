85def58c1f0aa1c620d5dc1d01cc18698e9fa718
Dave Brooks <dave@bcs.co.nz>
2013-04-29 18:38:55 +1200

    Update copyright years.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'BioSignalML Python API'
-copyright = u'2012, David Brooks'
+copyright = u'2011-2013, David Brooks'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
