0e7ccd5c950990573139fea87d552ba2641b6082
David Brooks <dave@bcs.co.nz>
2014-07-22 13:38:12 +1200

    Specify `rdflib` package in setup.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='BioSignalML',
                         'httplib2 >= 0.7.7',
                         'python-dateutil >= 2.0',
                         'ws4py >= 0.2.4',
+                        'rdflib >= 4.1.0',
                         'pint == 0.1.3-djb',
                        ],
       dependency_links=['http://github.com/dbrnz/pint/tarball/master#egg=pint-0.1.3-djb']
