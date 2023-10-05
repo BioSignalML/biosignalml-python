0c21adc30347b17928ae687a059fe6c0cd7d20a7
David Brooks <d.brooks@auckland.ac.nz>
2023-10-05 20:46:51 +1300

    Use `samplerate2` Python package instead of wrapping `libsamplerate`.

--- biosignalml/data/convert.py
+++ biosignalml/data/convert.py
@@ -18,73 +18,29 @@
 #
 ######################################################
 
-import random
+from enum import IntEnum
+
 import numpy as np
-import math
-import ctypes as C
-import logging
+import samplerate2 as samplerate
 
 __all__ = [ 'RateConverter', 'ConvertError' ]
 
 #===============================================================================
 
-SINC_BEST_QUALITY   = 0
-SINC_MEDIUM_QUALITY = 1
-SINC_FASTEST        = 2
-ZERO_ORDER_HOLD     = 3
-LINEAR              = 4
+class ResampleMethod(IntEnum):
+  SINC_BEST_QUALITY   = 0
+  SINC_MEDIUM_QUALITY = 1
+  SINC_FASTEST        = 2
+  ZERO_ORDER_HOLD     = 3
+  LINEAR              = 4
 
-#===============================================================================
-
-class _SRC_STATE(C.Structure):
-  pass
-
-#===============================================================================
-
-class _SRC_DATA(C.Structure):
-  _fields_ = [ ('data_in',           C.POINTER(C.c_float)),
-               ('data_out',          C.POINTER(C.c_float)),
-               ('input_frames',      C.c_long),
-               ('output_frames',     C.c_long),
-               ('input_frames_used', C.c_long),
-               ('output_frames_gen', C.c_long),
-               ('end_of_input',      C.c_int),
-               ('src_ratio',         C.c_double),
-             ]
-
-#===============================================================================
-
-try:
-  _srclib = C.CDLL('libsamplerate.dylib')
-except OSError:
-  try:
-    _srclib = C.CDLL('libsamplerate.so')
-  except OSError:
-    try:
-      _srclib = C.CDLL('libsamplerate.dll')
-    except OSError:
-      raise OSError("Unable to load libsamplerate library")
-
-src_new = _srclib.src_new
-src_new.argtypes = [C.c_int, C.c_int, C.POINTER(C.c_int)]
-src_new.restype = C.POINTER(_SRC_STATE)
-
-src_strerror = _srclib.src_strerror
-src_strerror.argtypes = [C.c_int]
-src_strerror.restype = C.c_char_p
-
-src_set_ratio = _srclib.src_set_ratio
-src_set_ratio.argtypes = [C.POINTER(_SRC_STATE), C.c_double]
-
-src_process = _srclib.src_process
-src_process.argtypes = [C.POINTER(_SRC_STATE), C.POINTER(_SRC_DATA)]
-src_process.restype = C.c_int
-
-src_delete = _srclib.src_delete
-src_delete.argtypes = [C.POINTER(_SRC_STATE)]
-
-
-MAX_OUTPUT_FRAMES = 50000       #: For each yielded data block
+_samplerate_method = {
+  ResampleMethod.SINC_BEST_QUALITY: samplerate.sinc_best,
+  ResampleMethod.SINC_MEDIUM_QUALITY: samplerate.sinc_medium,
+  ResampleMethod.SINC_FASTEST: samplerate.sinc_fastest,
+  ResampleMethod.ZERO_ORDER_HOLD: samplerate.zero_order_hold,
+  ResampleMethod.LINEAR: samplerate.linear
+}
 
 #===============================================================================
 
@@ -97,53 +53,19 @@ class ConvertError(Exception):
 class RateConverter(object):
 #===========================
 
-  def __init__(self, rate, channels=1, maxoutput=MAX_OUTPUT_FRAMES, method=SINC_MEDIUM_QUALITY):
-  #---------------------------------------------------------------------------------------------
-    error = C.c_int()
-    self._converter = src_new(method, channels, C.byref(error))
-    if not self._converter: raise ConvertError(src_strerror(error))
-    self.rate = float(rate)
-    self._channels = channels
-    self._maxoutframes = maxoutput
-
-  def __del__(self):
-  #-----------------
-    if self._converter:
-      src_delete(self._converter)
+  def __init__(self, output_rate, channels=1, method=ResampleMethod.SINC_MEDIUM_QUALITY):
+  #--------------------------------------------------------------------------------------
+    self.__output_rate = float(output_rate)
+    self.__resampler = samplerate.Resampler(_samplerate_method[method], channels)
 
   def convert(self, data, rate=None, finished=False):
   #--------------------------------------------------
-    if data is not None:
-      self._inshape = data.shape
-      inframes = self._inshape[0]
-      assert(self._channels == data.size/inframes)
-    else:
+    if data is None:
       data = np.array([0])
-      inframes = 0
     input = data.flatten().astype(np.float32)
     assert(input.flags['CONTIGUOUS'])
-    inpos = 0
-    outframes = 1 # For first iteration, actual value set below 
-    while inframes > 0 or finished and outframes > 0:
-      data = _SRC_DATA()
-      data.data_in = input[inpos:].ctypes.data_as(C.POINTER(C.c_float))
-      data.input_frames = inframes
-      data.end_of_input = 1 if finished else 0
-      if rate is not None: data.src_ratio = self.rate/rate
-      else:                data.src_ratio = 1.0
-      output = np.zeros(self._channels*self._maxoutframes, dtype=np.float32)
-      data.data_out = output.ctypes.data_as(C.POINTER(C.c_float))
-      data.output_frames = self._maxoutframes
-
-      error = src_process(self._converter, data)
-      if error: raise ConvertError(src_strerror(error))
-
-      inframes -= data.input_frames_used
-      inpos += self._channels*data.input_frames_used
-      outframes = data.output_frames_gen
-      outshape = (outframes,) + self._inshape[1:]
-      if outframes > 0:
-        yield output[:self._channels*outframes].reshape(outshape)
+    ratio = self.__output_rate/rate if rate is not None else 1.0
+    yield self.__resampler.process(input, ratio, finished)
 
 #===============================================================================
 
@@ -155,7 +77,7 @@ if __name__ == "__main__":
 
 #  print signal
 
-  resampler = RateConverter(2, maxoutput=50)
+  resampler = RateConverter(2)
   #for o in resampler.convert(signal, 1, finished=True):
   g = resampler.convert(signal, 1, finished=True)
   for o in g:
