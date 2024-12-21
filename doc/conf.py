9ce07ba044927696010fe60bfdb2512a35531b76
David Brooks <d.brooks@auckland.ac.nz>
2024-12-22 12:36:15 +1300

    Use `furo` Sphinx theme for documentation and `autoapi` instead of `autodoc`.

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,9 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+
+autoapi_dirs = ['../biosignalml']
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -29,7 +32,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'autoapi.extension',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
     'sphinx.ext.graphviz',
@@ -104,7 +107,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'  # 'alabaster' # 'classic'
+html_theme = 'furo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- doc/makedoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm biosignalml.*.rst
-sphinx-apidoc -T  -o . ../biosignalml
-
-make clean html
